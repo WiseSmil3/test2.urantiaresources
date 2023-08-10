1c8dc05a8191cbaa5979f777843fd8aa4fd3aca5
WiseSmil3 <86897895+WiseSmil3@users.noreply.github.com>
2023-08-10 18:50:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,70 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    fetch('resources.json')
-        .then(response => response.json())
-        .then(data => {
-            const resourcesElement = document.getElementById('resources');
-            data.forEach(resource => {
-                const resourceCard = document.createElement('div');
-                resourceCard.classList.add('resource-card');
-                resourceCard.innerHTML = `
-                    <h2>${resource.name}</h2>
-                    <p>${resource.description}</p>
-                    <a href="${resource.url}" target="_blank">Go to resource</a>
-                `;
-                resourcesElement.appendChild(resourceCard);
-            });
-        });
+// JavaScript code for the header effects on scroll
+window.addEventListener("scroll", function () {
+  var header = document.querySelector(".header");
+  header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+// JavaScript code for the responsive navigation sidebar menu
+var menuToggle = document.querySelector(".toggle");
+var menu = document.querySelector(".menu");
+var icons = document.querySelector(".icons");
+
+menuToggle.addEventListener("click", function () {
+  menuToggle.classList.toggle("active");
+  menu.classList.toggle("active");
+  icons.classList.toggle("active");
+});
+
+// JavaScript code for the image slider
+var slides = document.querySelectorAll(".slide");
+var btns = document.querySelectorAll(".btn");
+let currentSlide = 1;
+
+// Javascript for image slider manual navigation
+var manualNav = function (manual) {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+
+    btns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+  });
+
+  slides[manual].classList.add("active");
+  btns[manual].classList.add("active");
+};
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    manualNav(i);
+    currentSlide = i;
+  });
+});
+
+// Javascript for image slider autoplay navigation
+var repeat = function (activeClass) {
+  let active = document.getElementsByClassName("active");
+  let i = 1;
+
+  var repeater = () => {
+    setTimeout(function () {
+      [...active].forEach((activeSlide) => {
+        activeSlide.classList.remove("active");
+      });
+
+      slides[i].classList.add("active");
+      btns[i].classList.add("active");
+      i++;
+
+      if (slides.length == i) {
+        i = 0;
+      }
+      if (i >= slides.length) {
+        return;
+      }
+      repeater();
+    }, 4000);
+  };
+  repeater();
+};
+repeat();
